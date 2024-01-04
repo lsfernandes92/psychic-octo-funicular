1a1354b9145da828614162580bde50624fcabbce
lsfernandes92 <lsfernandes92@gmail.com>
2024-01-04 14:29:38 -0300

    Generate only desired tests

--- config/application.rb
+++ config/application.rb
@@ -40,5 +40,14 @@ module MyApp
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    # Generate only desired tests
+    config.generators do |g|
+      g.test_framework :rspec,
+      fixtures: false,
+      view_specs: false,
+      helper_specs: false,
+      routing_specs: false
+    end
   end
 end
