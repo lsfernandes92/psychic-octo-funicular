79cd72163e5413787381f69061355411454ac212
lsfernandes92 <lsfernandes92@gmail.com>
2024-01-05 13:08:14 -0300

    Add Order model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_05_145317) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_05_160659) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -76,7 +76,16 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_05_145317) do
     t.integer "days_to_pay"
   end
 
+  create_table "orders", force: :cascade do |t|
+    t.string "description"
+    t.bigint "customer_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["customer_id"], name: "index_orders_on_customer_id"
+  end
+
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "comments", "articles"
+  add_foreign_key "orders", "customers"
 end
