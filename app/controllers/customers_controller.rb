238f744df63a9d065c7805a2103b56dfc5bcf4ba
lsfernandes92 <lsfernandes92@gmail.com>
2024-01-19 12:36:39 -0300

    Add test sample on how to test requests

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,2 +1,33 @@
 class CustomersController < ApplicationController
+  before_action :set_customer, only: [:show]
+
+  def index
+    @customers = Customer.all
+    render json: @customers
+  end
+
+  def show
+    render json: @customer
+  end
+
+  def create
+    @customer = Customer.new(customer_params)
+    
+    if @customer.save
+      render json: @customer, status: :created
+    else
+      render json: @customer.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+    def set_customer
+      byebug
+      @customer = Customer.find(params[:id])
+    end
+
+    def customer_params
+      params.permit(:name, :email)
+    end
 end
--- spec/requests/customers_spec.rb
+++ spec/requests/customers_spec.rb
@@ -1,7 +1,84 @@
 require 'rails_helper'
 
-RSpec.describe "Customers", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe 'Customers', type: :request do
+  before { create(:customer) }
+
+  describe 'GET #index' do
+    before { get customers_path }
+
+    it 'responds successfully' do
+      expect(response).to have_http_status :ok
+    end
+  end
+  
+  describe 'GET #show' do
+    let(:customer_id) { Customer.last.id }
+
+    before { get customer_path(customer_id) }
+
+    it 'returns customers as json' do
+      expect(response_body).to be_an Hash 
+      expect(response_body).to include_json(
+        id: /\d/,
+        name: (be_kind_of String),
+        email: (be_kind_of String)
+      )
+    end
+  end
+
+  describe 'POST #create' do
+    context 'with valid customer' do
+      let(:customer_params) do
+        attributes_for(
+          :customer,
+          name: 'Foo name',
+          email: 'Foo email'
+        )
+      end
+
+      before { post(customers_path, params: customer_params) }
+
+      it 'responds with :created' do
+        expect(response).to have_http_status :created
+      end
+
+      it 'creates customer successfully' do
+        expect(response_body).to be_an Hash
+        expect(response_body).to include_json(
+          id: /\d/,
+          name: 'Foo name',
+          email: 'Foo email'
+        )
+      end
+    end
+
+    context 'with invalid customer' do
+      let(:invalid_customer_params) do
+        attributes_for(
+          :customer,
+          name: '',
+          email: ''
+        )
+      end
+
+      before { post(customers_path, params: invalid_customer_params) }
+
+      it 'responds with :unprocessable_entity' do
+        expect(response).to have_http_status :unprocessable_entity
+      end
+
+      it 'responds with validations' do
+        expect(response_body).to include_json(
+          name: ["can't be blank"],
+          email: ["can't be blank"]
+        )
+      end
+    end
   end
 end
+
+private
+
+  def response_body
+    JSON.parse(response.body)
+  end
